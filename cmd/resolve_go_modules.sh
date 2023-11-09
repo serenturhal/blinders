1f0ee30058b8fb4cca6dbaf2b23f553e5449b24f
Tan Le <tanlethanh8266@gmail.com>
2023-11-09 19:12:58 +0700

    feat: add resolve go modules to run ci

---
+++ cmd/resolve_go_modules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Recursively finds all directories with a go.mod file and creates
+# a GitHub Actions JSON output option. This is used by the linter action.
+
+echo "Resolving modules in $(pwd)"
+
+PATHS=$(find . -mindepth 2 -type f -name go.mod -execdir printf '{"workdir":"%s"},'  "$PWD" \;)
+# PATHS=$(find . -mindepth 2 -type f -name go.mod -printf '{"workdir":"%h"},')
+
+echo "::set-output name=matrix::{\"include\":[${PATHS%?}]}"
