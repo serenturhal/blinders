63df7e31a88387cedc227ea65b2dd7e74a30a942
Tan Le <tanlethanh8266@gmail.com>
2023-11-19 06:11:51 +0700

    feat: implement translation lambda

--- functions/translation/main.go
+++ functions/translation/main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"blinders/packages/translation"
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type TranslatePayload struct {
+	Text string `json:"text"`
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
-	if event == nil {
-		return nil, fmt.Errorf("received nil event")
+type TranslateResponse struct {
+	Text       string `json:"text"`
+	Translated string `json:"translated"`
+	Languages  string `json:"languages"`
+}
+
+var translator translation.Translator
+
+func init() {
+	translator = translation.YandexTranslator{ApiKey: os.Getenv("YANDEX_API_KEY")}
+	fmt.Println(translator, "<-- translator")
+}
+
+func HandleRequest(_ context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	fmt.Println(event, "<-- event")
+	translated, err := translator.TranslateEnToVi("hello")
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: 400,
+			Body:       err.Error(),
+		}, err
 	}
-	message := fmt.Sprintf("Hello %s, from translation function!", event.Name)
-	return &message, nil
+
+	res := TranslateResponse{
+		Text:       "hello",
+		Translated: translated,
+		Languages:  "en-vi",
+	}
+
+	resInBytes, _ := json.Marshal(res)
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: 200,
+		Body:       string(resInBytes),
+	}, nil
 }
 
 func main() {
