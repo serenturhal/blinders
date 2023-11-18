eb084497e4fcf9d264c47d5ffa5aaaccf0765639
Tan Le <tanlethanh8266@gmail.com>
2023-11-19 05:26:54 +0700

    chore: clean dictionary function

--- functions/dictionary/__main__.py
+++
@@ -1,5 +0,0 @@
-from blinders.dictionary_core import handle_dictionary
-
-if __name__ == "__main__":
-    print(handle_dictionary)
-    handle_dictionary()
--- functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
+++ functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
@@ -8,5 +8,5 @@ def call_handle_dictionary():
 
 def lambda_handler(event, context):
     """Example of calling a function from another module."""
-    print("dictionary on event", event, context)
-    return call_handle_dictionary()
+    print("dictionary on event", event["resource"])
+    return {"statusCode": 200, "body": call_handle_dictionary()}
