927d303734894a986a1e88f2dfb5790a6422c815
hnimtadd <minhdat15012002@gmail.com>
2023-11-21 20:43:09 +0700

    chore: fix go mod issues

--- functions/authentication/go.mod
+++ functions/authentication/go.mod
@@ -1,3 +1,5 @@
 module blinder/functions/authentication
 
 go 1.21.3
+
+require github.com/aws/aws-lambda-go v1.41.0
--- packages/authentication/main.go
+++ packages/authentication/main.go
@@ -1,52 +1,53 @@
 package authentication
 
-import (
-	"os"
-	"time"
-)
-
-// Public function to outter scope of package
-
-var Manager Maker
-
-func init() {
-	if Manager == nil {
-		secretKey := os.Getenv("JWT_SECRET")
-		durationString := os.Getenv("JWT_DURATION")
-		tokenDuration, err := time.ParseDuration(durationString)
-		if err != nil {
-			Manager, err = NewJWTManager(JwtOptions{
-				SecretKey: secretKey,
-			})
-			if err != nil {
-				panic(err)
-			}
-			return
-		}
-		Manager, err = NewJWTManager(JwtOptions{
-			SecretKey:     secretKey,
-			TokenDuration: tokenDuration,
-		})
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-// Generate jwtToken from userID and userEmail
-func GenerateTokenForUser(userID string, userEmail string) (string, error) {
-	user := &User{
-		ID:    userID,
-		Email: userEmail,
-	}
-	return Manager.Generate(user)
-}
-
-// Verify jwtToken, return userID, userEmail, error respectively
-func VerifyToken(token string) (string, string, error) {
-	user, err := Manager.Verify(token)
-	if err != nil {
-		return "", "", err
-	}
-	return user.ID, user.Email, nil
-}
+//
+// import (
+// 	"os"
+// 	"time"
+// )
+//
+// // Public function to outter scope of package
+//
+// var Manager Maker
+//
+// func init() {
+// 	if Manager == nil {
+// 		secretKey := os.Getenv("JWT_SECRET")
+// 		durationString := os.Getenv("JWT_DURATION")
+// 		tokenDuration, err := time.ParseDuration(durationString)
+// 		if err != nil {
+// 			Manager, err = NewJWTManager(JwtOptions{
+// 				SecretKey: secretKey,
+// 			})
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			return
+// 		}
+// 		Manager, err = NewJWTManager(JwtOptions{
+// 			SecretKey:     secretKey,
+// 			TokenDuration: tokenDuration,
+// 		})
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	}
+// }
+//
+// // Generate jwtToken from userID and userEmail
+// func GenerateTokenForUser(userID string, userEmail string) (string, error) {
+// 	user := &User{
+// 		ID:    userID,
+// 		Email: userEmail,
+// 	}
+// 	return Manager.Generate(user)
+// }
+//
+// // Verify jwtToken, return userID, userEmail, error respectively
+// func VerifyToken(token string) (string, string, error) {
+// 	user, err := Manager.Verify(token)
+// 	if err != nil {
+// 		return "", "", err
+// 	}
+// 	return user.ID, user.Email, nil
+// }
