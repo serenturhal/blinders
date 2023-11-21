832357b41110ad6cf4dc66009b0ea36865ce89bc
hnimtadd <minhdat15012002@gmail.com>
2023-11-21 15:59:17 +0700

    refactor: refactor authentication service

--- services/authentication/main.go
+++ services/authentication/main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"blinders/packages/authentication"
 	"fmt"
+	"time"
+
+	"os"
+)
+
+var (
+	defaultTokenDuration = time.Minute * 15
 )
 
 func main() {
-	token, err := authentication.GenerateTokenForUser("t7ZYtyjYCbMxOefUALu8b2P4AVO2", "minhdat15012002@gmail.com")
+	secretKey := os.Getenv("JWT_SECRET")
+	durationString := os.Getenv("JWT_DURATION")
+	tokenDuration, err := time.ParseDuration(durationString)
+	if err != nil {
+		tokenDuration = defaultTokenDuration
+	}
+
+	maker, err := authentication.NewJWTManager(authentication.JwtOptions{
+		SecretKey:     secretKey,
+		TokenDuration: tokenDuration,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	token, err := maker.Generate(&authentication.User{
+		ID:    "t7ZYtyjYCbMxOefUALu8b2P4AVO2",
+		Email: "minhdat15012002@gmail.com",
+	})
+
 	if err != nil {
 		panic(err)
 	}
