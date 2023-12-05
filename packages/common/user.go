e8f681199113149f556bad46959c2c3218da8cab
hnimtadd <minhdat15012002@gmail.com>
2023-12-06 00:12:54 +0700

    refactor: move userdata to common/user file

--- packages/common/user.go
+++ packages/common/user.go
@@ -16,3 +16,16 @@ func (u User) String() string {
 	str.WriteString(fmt.Sprintf("Email: %s", u.Email))
 	return str.String()
 }
+
+type UserData struct {
+	UserID   string   `json:"userID"`
+	Native   Language `json:"nativeLanguage"`
+	Learning Language `json:"learningLanguage"`
+}
+
+func (d UserData) String() string {
+	str := strings.Builder{}
+	str.WriteString(fmt.Sprintf("Native language: %s\n", d.Native))
+	str.WriteString(fmt.Sprintf("Learning language: %s\n", d.Native))
+	return str.String()
+}
