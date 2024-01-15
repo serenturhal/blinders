dfd9561d7f9813ed5096b83741da27903a34357c
Tan Le <tanlethanh8266@gmail.com>
2024-01-15 14:37:58 +0700

    chore: rename translate lambda

--- infra/route.tf
+++ infra/route.tf
@@ -26,30 +26,30 @@ output "get_dictionary_api" {
   value = "https://${aws_apigatewayv2_api_mapping.blinders_v1.domain_name}/${aws_apigatewayv2_api_mapping.blinders_v1.api_mapping_key}/dictionary"
 }
 
-# translation route
-resource "aws_apigatewayv2_integration" "blinders_translation" {
+# translate route
+resource "aws_apigatewayv2_integration" "blinders_translate" {
   api_id = aws_apigatewayv2_api.blinders.id
 
-  integration_uri  = aws_lambda_function.blinders_translation.invoke_arn
+  integration_uri  = aws_lambda_function.blinders_translate.invoke_arn
   integration_type = "AWS_PROXY"
 }
 
-resource "aws_apigatewayv2_route" "get_transblinders_translation" {
+resource "aws_apigatewayv2_route" "get_transblinders_translate" {
   api_id = aws_apigatewayv2_api.blinders.id
 
-  route_key = "GET /translation"
-  target    = "integrations/${aws_apigatewayv2_integration.blinders_translation.id}"
+  route_key = "GET /translate"
+  target    = "integrations/${aws_apigatewayv2_integration.blinders_translate.id}"
 }
 
-resource "aws_lambda_permission" "blinders_translation" {
+resource "aws_lambda_permission" "blinders_translate" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.blinders_translation.function_name
+  function_name = aws_lambda_function.blinders_translate.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.blinders.execution_arn}/*/*"
 }
 
-output "get_translation_api" {
-  value = "https://${aws_apigatewayv2_api_mapping.blinders_v1.domain_name}/${aws_apigatewayv2_api_mapping.blinders_v1.api_mapping_key}/translation"
+output "get_translate_api" {
+  value = "https://${aws_apigatewayv2_api_mapping.blinders_v1.domain_name}/${aws_apigatewayv2_api_mapping.blinders_v1.api_mapping_key}/translate"
 }
