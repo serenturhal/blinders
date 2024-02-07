6f8a84d158286fa8af0fabc86d9ed644ff9de504
Dat Nguyen <minhdat15012002@gmail.com>
2024-02-07 12:16:40 +0700

    feat: matchs suggest with mock package

--- packages/match/main.go
+++ packages/match/main.go
@@ -1,9 +1,15 @@
 package match
 
 type UserMatch struct {
-	UserID string
+	UserID   string
+	Name     string
+	Gender   string
+	Native   string
+	Learning string
+	Age      int
 }
 
 type Matcher interface {
 	Match(fromID string, toID string) error
+	Suggest(id string) ([]UserMatch, error)
 }
--- packages/match/mock.go
+++ packages/match/mock.go
@@ -5,3 +5,7 @@ type MockMatcher struct{}
 func (m *MockMatcher) Match(_ string, _ string) error {
 	return nil
 }
+
+func (m *MockMatcher) Suggest(id string) ([]UserMatch, error) {
+	return []UserMatch{}, nil
+}
