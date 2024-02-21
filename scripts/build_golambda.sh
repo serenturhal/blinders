ee6108490ee8afaa5e805d4a5708e1cbf543e5f7
Tan Le <tanlethanh8266@gmail.com>
2024-02-21 23:04:26 +0700

    chore: add echo logs for building go lambda functions

--- scripts/build_golambda.sh
+++ scripts/build_golambda.sh
@@ -1,5 +1,10 @@
 # !/bin/bash
 
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/translate
+echo "build translate completed"
+
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/websocket/connect
+echo "build connect completed"
+
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/websocket/disconnect
+echo "build disconnect completed"
