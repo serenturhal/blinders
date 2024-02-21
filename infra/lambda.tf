9e1b05e5aa820b15978a30be60fb9b4b7b5a8704
Tan Le <tanlethanh8266@gmail.com>
2024-02-21 23:03:16 +0700

    chore: refactor

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "dictionary" {
-  runtime          = "python3.10"
-  filename         = "../functions/dictionary/lambda_bundle.zip"
   function_name    = "blinders_dictionary"
+  filename         = "../functions/dictionary/lambda_bundle.zip"
   handler          = "blinders.dictionary_aws_lambda_function.lambda_handler"
-  role             = aws_iam_role.lambda_role.arn
-  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   source_code_hash = filebase64sha256("../functions/dictionary/lambda_bundle.zip")
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.10"
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
 resource "null_resource" "translate" {
