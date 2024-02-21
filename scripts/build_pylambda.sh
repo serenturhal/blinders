6ffb5c7d2b59a3a3ac1f68900421da5e822da0ae
Tan Le <tanlethanh8266@gmail.com>
2024-02-21 17:19:16 +0700

    chore: add flags and comments

--- scripts/build_pylambda.sh
+++ scripts/build_pylambda.sh
@@ -1,3 +1,6 @@
+# !/bin/bash
+
+# this build file is used for single module builder
 poetry build
 poetry run pip install --upgrade -t bundle dist/*.whl
 cd bundle ; zip -r ../lambda_bundle.zip . -x '*.pyc'
