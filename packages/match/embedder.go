99b12867e5ad3c857469870bbad03a0e6cff6e98
Dat Nguyen <minhdat15012002@gmail.com>
2024-02-21 12:54:55 +0700

    refactor: mock embedder uses rand to generate random embed vector

--- packages/match/embedder.go
+++ packages/match/embedder.go
@@ -1,5 +1,10 @@
 package match
 
+import (
+	"math/rand"
+	"time"
+)
+
 // Embedder uses embedding model to embed user information and return embedding vector.
 type Embedder interface {
 	Embed(info UserMatch) ([]float32, error)
@@ -8,5 +13,10 @@ type Embedder interface {
 type MockEmbedder struct{}
 
 func (e MockEmbedder) Embed(_ UserMatch) ([]float32, error) {
-	return []float32{}, nil
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	embed := make([]float32, 10)
+	for i := range embed {
+		embed[i] = r.Float32()
+	}
+	return embed, nil
 }
