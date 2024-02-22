6c694163408a3fc874fbcb482d9e6def55b4392b
Tan Le <tanlethanh8266@gmail.com>
2024-02-22 23:30:47 +0700

    feat: complete ws authorizer with firebase auth manager

--- functions/websocket/authorizer/main.go
+++ functions/websocket/authorizer/main.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"blinders/packages/auth"
+	"blinders/packages/utils"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,10 +19,41 @@ type APIGatewayWebsocketProxyRequest struct {
 	MethodArn                              string `json:"methodArn"` // ??? refs: https://gist.github.com/praveen001/1b045d1c31cd9c72e4e6638e9f883f83
 }
 
+var authManager auth.Manager
+
+func init() {
+	adminConfig, err := utils.GetFile("firebase.admin.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authManager, err = auth.NewFirebaseManager(adminConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func HandleRequest(
 	_ context.Context,
 	request APIGatewayWebsocketProxyRequest,
 ) (events.APIGatewayCustomAuthorizerResponse, error) {
+	authorization := request.Headers["Authorization"]
+	if !strings.Contains(authorization, "Bearer ") {
+		// TODO: need to response with unauthorized response code
+		// (it's currently 500 as default)
+		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("[authorizer] invalid token")
+	}
+	jwt := strings.Split(authorization, " ")[1]
+	user, err := authManager.Verify(jwt)
+	if err != nil {
+		return events.APIGatewayCustomAuthorizerResponse{}, err
+	}
+
+	// Is it secure to log the uid out to cloudwatch?
+	// how to log the request tracking efficient and secure
+	log.Println("[authorizer] issued user's policy of", user.AuthID)
+
+	userBytes, _ := json.Marshal(user)
 	return events.APIGatewayCustomAuthorizerResponse{
 		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
 			Version: "2012-10-17",
@@ -28,7 +66,7 @@ func HandleRequest(
 			},
 		},
 		Context: map[string]interface{}{
-			"user": "hello user from authorizer",
+			"user": string(userBytes),
 		},
 	}, nil
 }
