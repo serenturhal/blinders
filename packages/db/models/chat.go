380186c1f4dc72adb10199ec427389aa3817394c
Tan Le <tanlethanh8266@gmail.com>
2024-02-24 18:23:52 +0700

    chore: remove messages out of conversion model

--- packages/db/models/chat.go
+++ packages/db/models/chat.go
@@ -5,7 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Conversation struct {
 	ID        primitive.ObjectID   `bson:"_id"       json:"Id"`
 	Members   []Member             `bson:"members"   json:"members"`
-	Messages  []Message            `bson:"messages"  json:"messages"`
 	CreatedAt primitive.DateTime   `bson:"createdAt" json:"createdAt"`
 	UpdatedAt primitive.DateTime   `bson:"updatedAt" json:"updatedAt"`
 	Metadata  ConversationMetadata `bson:"metadata"  json:"metadata"`
@@ -26,14 +25,24 @@ type Member struct {
 	JoinedAt              primitive.DateTime `bson:"joinedAt"              json:"joinedAt"`
 }
 
+type MessageStatus string
+
+const (
+	DeliveredStatus MessageStatus = "delivered"
+	ReceivedStatus  MessageStatus = "received"
+	SeenStatus      MessageStatus = "seen"
+)
+
 type Message struct {
-	ID        primitive.ObjectID `bson:"_id"       json:"Id"`
-	SenderID  primitive.ObjectID `bson:"senderId"  json:"senderId"`
-	Content   string             `bson:"content"   json:"content"`
-	Status    string             `bson:"status"    json:"status"`
-	CreatedAt primitive.DateTime `bson:"createdAt" json:"createdAt"`
-	UpdatedAt primitive.DateTime `bson:"updatedAt" json:"updatedAt"`
-	Emotions  []MessageEmotion   `bson:"emotions"  json:"emotions"`
+	ID             primitive.ObjectID `bson:"_id"            json:"Id"`
+	SenderID       primitive.ObjectID `bson:"senderId"       json:"senderId"`
+	ConversationID primitive.ObjectID `bson:"conversationId" json:"conversationId"`
+	ReplyTo        primitive.ObjectID `bson:"replyTo"        json:"replyTo"`
+	Content        string             `bson:"content"        json:"content"`
+	Status         MessageStatus      `bson:"status"         json:"status"`
+	CreatedAt      primitive.DateTime `bson:"createdAt"      json:"createdAt"`
+	UpdatedAt      primitive.DateTime `bson:"updatedAt"      json:"updatedAt"`
+	Emotions       []MessageEmotion   `bson:"emotions"       json:"emotions"`
 }
 
 type MessageEmotion struct {
