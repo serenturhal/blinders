e51d50506feaa16ff682ba494423c90349a367d5
Tan Le <tanlethanh8266@gmail.com>
2024-03-10 16:40:45 +0700

    chore: add jwt Bearer prefix

--- packages/goauth/fiber.go
+++ packages/goauth/fiber.go
@@ -1,7 +1,8 @@
 package auth
 
 import (
-	"fmt"
+	"log"
+	"strings"
 
 	"blinders/packages/db/repo"
 
@@ -14,20 +15,28 @@ const UserAuthKey key = "user_auth_key"
 
 func FiberAuthMiddleware(m Manager, userRepo *repo.UsersRepo) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		jwt := ctx.Get("Authorization")
+		auth := ctx.Get("Authorization")
+		if !strings.HasPrefix(auth, "Bearer ") {
+			return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "invalid jwt, missing bearer token",
+			})
+		}
+
+		jwt := strings.Split(auth, " ")[1]
 		user, err := m.Verify(jwt)
 		if err != nil {
+			log.Println("failed to verify jwt", err)
 			return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": err.Error(),
+				"message": "failed to verify jwt",
 			})
 		}
 
 		// currently, user.AuthID is firebaseUID
 		usr, err := userRepo.GetUserByFirebaseUID(user.AuthID)
 		if err != nil {
-			fmt.Println(err)
+			log.Println("failed to get user", err)
 			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": "cannot get user " + err.Error(),
+				"message": "failed to get user",
 			})
 		}
 
